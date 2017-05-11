87fb74829a3a67bcec5cc60330b3bb4ec6a7492b
Laurel Ruhlen <lruhlen@spenceinfo.spins.com>
2017-05-11 13:18:53 -0500

    Increment version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='deep_nest',
-      version='0.12',
+      version='0.13',
       description='A sane approach to arbitrarily nested dictionaries (*cough* nested JSON *cough*)',
       url='https://github.com/lruhlen/deep_nest',
       author='L. Ruhlen',
